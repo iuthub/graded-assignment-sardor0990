b4f4658722331c3e077cb0d1697d6d84970efdaf
sardor0990 <sardorzufarov98@gmail.com>
2020-04-17 20:55:02 +0300

    finished all tasks

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::resource('/tasks','TaskController')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
